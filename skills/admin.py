1296169ae3a58a2ab835a1c3bb322e4b950e1cf4
Evgeny <krivosheevevgeny@gmail.com>
2015-06-03 15:41:00 +0300

    create-skillratelog-model

--- skills/admin.py
+++ skills/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from skills.models import Skill, SkillRate
+from skills.models import Skill, SkillRate, SkillRateLog
 
 
 @admin.register(Skill)
@@ -10,3 +10,8 @@ class SkillAdmin(admin.ModelAdmin):
 @admin.register(SkillRate)
 class SkillRateAdmin(admin.ModelAdmin):
     list_display = ['user_id', 'skill_id', 'self_rate', 'guests_rate']
+
+
+@admin.register(SkillRateLog)
+class SkillRateLogAdmin(admin.ModelAdmin):
+    list_display = ['user_id', 'skill_id', 'rate', 'date']
--- skills/models.py
+++ skills/models.py
@@ -30,3 +30,18 @@ class SkillRate(models.Model):
 
     def __str__(self):
         return 'user: {user_id}, skill: {skill_id}'.format(user_id=self.user_id, skill_id=self.skill_id)
+
+
+@python_2_unicode_compatible
+class SkillRateLog(models.Model):
+    user_id = models.ForeignKey(ExtUser, primary_key=True, verbose_name=_('ExtUser'), related_name='skill_rate_log')
+    skill_id = models.ForeignKey(SkillRate, verbose_name=_('Skill'), related_name='skill_rate_log')
+    rate = IntegerRangeField(verbose_name=_("User's rate"), min_value=1, max_value=10)
+    date = models.DateTimeField(verbose_name=_('Date'), auto_now_add=True)
+
+    class Meta:
+        verbose_name = _('Skill Rate Log')
+        unique_together = ('user_id', 'skill_id')
+
+    def __str__(self):
+        return 'skill: {skill_id}, rate: {rate}'.format(skill_id=self.skill_id, rate=self.rate)
