7c3899cfe9620488db27cf082ab7dc35c308a51c
Evgeny <krivosheevevgeny@gmail.com>
2015-06-08 13:17:43 +0300

    add-token-refresh-endpoint

--- api/urls.py
+++ api/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^auth', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^auth/login', 'rest_framework_jwt.views.obtain_jwt_token'),
+    url(r'^auth/refresh', 'rest_framework_jwt.views.refresh_jwt_token'),
     url(r'^auth/register', UserRegister.as_view()),
 ]
