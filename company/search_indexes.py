8e8cc4e1b095bf240da409b099b4632bffa55435
Evgeny <krivosheevevgeny@gmail.com>
2015-06-15 11:51:21 +0300

    add-job-index

--- company/search_indexes.py
+++ company/search_indexes.py
@@ -1,5 +1,5 @@
 from haystack import indexes
-from .models import Company
+from .models import Company, Job
 
 
 class CompanyIndex(indexes.SearchIndex, indexes.Indexable):
@@ -15,6 +15,23 @@ class CompanyIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_model(self):
         return Company
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.all()
+
+
+class JobIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    name = indexes.CharField(model_attr='name')
+    description = indexes.CharField(model_attr='description')
+    salary = indexes.FloatField(model_attr='salary')
+    added_at = indexes.DateTimeField(model_attr='added_at')
+    last_change = indexes.DateTimeField(model_attr='last_change')
+    company = indexes.CharField(model_attr='company')
+
+    def get_model(self):
+        return Job
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
